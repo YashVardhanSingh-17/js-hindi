75021efe062d052cd130ca2af658aee1bcf9a590
Yash Vardhan Singh <yashvardhansingh9615@gmail.com>
2024-09-15 14:46:41 +0000

    datatypes

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(3+3)
+
+console.log("Yash")
+
+let name= "Yash"
+let age= 22
+let isLoggedIn = FontFaceSetLoadEvent
+let state;
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true or false
+//null => standalone value
+// undefined => 
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof "Yash");
