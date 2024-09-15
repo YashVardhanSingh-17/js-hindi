1a8155542f2d4f0bf4f5eb248d98ad3399a333b4
Yash Vardhan Singh <yashvardhansingh9615@gmail.com>
2024-09-15 14:47:20 +0000

    variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,6 @@
+ const account_Id = 26960
+ let accountEmail = "yash@gmail.com "
+ var accountPassword = "12345"
+ accountCity = "Lucknow"
+ 
+ console.table([account_Id, accountEmail, accountPassword, accountCity])
